ed6a64eec1de1e79efda82cbad425a42b382c8ef
Anna Zhukova <anna.zhukanna@gmail.com>
2022-04-29 03:33:50 +0500

    feat: add preloader to backlog

--- src/index.js
+++ src/index.js
@@ -8,14 +8,30 @@ import dnd from './modules/dnd.js';
 
 const backlogContainer = document.querySelector('.backlog_task-list');
 
+const preloader = document.createElement('div');
+preloader.classList.add('preloader');
+preloader.innerHTML = '<div class="animated-background"></div>';
+for (let i = 1; i < 15; i++) {
+  backlogContainer.append(preloader.cloneNode(true));
+}
+
 getTasks()
   .then((tasks) => {
+    backlogContainer.innerHTML = '';
     return tasks.map((item) => {
       let task = document.createElement('div');
       task.classList.add('backlog_task');
       task.draggable = true;
       task.innerText = item.subject;
-      backlogContainer.append(task);
+      task.setAttribute('data-start', item.planStartDate);
+      task.setAttribute('data-end', item.planEndDate);
+      if (item.executor) {
+        task.classList.add('task');
+        document.querySelector(`.cells_item[data-user='${item.executor}']`).append(task);
+      } else {
+        backlogContainer.append(task);
+        //добавить размещение на несколько дат
+      }
     });
   })
-  .then((onfulfilled) => dnd());
+  .then(() => dnd());
