a0325fd1bcd3e6580a72358c9657efa7dfd6d67e
Anna Zhukova <anna.zhukanna@gmail.com>
2022-04-28 17:11:14 +0500

    feat: add function to switch weeks

--- src/modules/timeline.js
+++ src/modules/timeline.js
@@ -2,20 +2,37 @@ const prevBtn = document.querySelector('.timeline_btn--prev');
 const nextBtn = document.querySelector('.timeline_btn--next');
 const timeline = document.querySelector('.timeline_wrapper');
 
-let currentDate = new Date();
-let options = { month: 'numeric', day: 'numeric' };
-let today = new Intl.DateTimeFormat('ru-RU', options).format(currentDate);
-//let week = [];
+const currentDate = new Date();
+const options = { month: 'numeric', day: 'numeric' };
+const today = currentDate.toLocaleString('ru', options);
+let weekNumber = 0;
 
-for (let i = 1; i <= 7; i++) {
-  let day = new Intl.DateTimeFormat('ru-RU', options).format(
-    currentDate.setDate(currentDate.getDate() - currentDate.getDay() + i)
-  );
-  //week.push(day);
-  let date = document.createElement('div');
-  day === today? date.classList.add('timeline_item','timeline_item--today') : date.classList.add('timeline_item');
-  date.innerText = day;
-  timeline.append(date);
-}
+const setWeek = (n) => {
+  const week = [];
+  timeline.innerHTML = '';
+  weekNumber += n;
 
-//console.log(today, week);
+  for (let i = 1; i <= 7; i++) {
+    let day = new Intl.DateTimeFormat('ru', options).format(
+      new Date().setDate(currentDate.getDate() - currentDate.getDay() + i + weekNumber)
+    );
+
+    week.push(day);
+
+    let date = document.createElement('div');
+    day === today
+      ? date.classList.add('timeline_item', 'timeline_item--today')
+      : date.classList.add('timeline_item');
+    date.setAttribute('data-date', day);
+    date.innerText = day;
+    timeline.append(date);
+  }
+  console.log(today, week);
+};
+
+setWeek(weekNumber);
+
+prevBtn.addEventListener('click', () => setWeek(-7));
+nextBtn.addEventListener('click', () => setWeek(7));
+
+// создать функцию для обновления data-date в ячейках задач
