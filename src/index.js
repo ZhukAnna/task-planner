eaaea0b9c21e6ccc7d1d5051b5292bc623393da0
Anna Zhukova <anna.zhukanna@gmail.com>
2022-05-11 14:12:34 +0500

    refactor: move styles in separate files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './css/normalize.css';
 import './css/style.css';
+import './css/timeline.css';
+import './css/users.css';
+import './css/cells.css';
+import './css/backlog.css';
+import './css/task.css';
+import './css/preloader.css';
 
 import './modules/users.js';
 import './modules/timeline.js';
